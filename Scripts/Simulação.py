17405002dbc773596d58fcd1ae3772978d36247b
Cristiano <ladrilhandodata@gmail.com>
2026-03-27 20:43:40 -0300

    Add LGPD compliance note to Simulação.py
    Added a note about data confidentiality and compliance with LGPD regulations.

--- Scripts/Simulação.py
+++ Scripts/Simulação.py
@@ -1,3 +1,6 @@
+# OBS: obedecendo as diretrizes da LGPD e a confidencialidade do Cliente, os dados desse script serão gerados de forma sintética,
+#mas sendo fidedignos ao processo.
+
 #Bibliotecas ==========================
 import random
 import time
